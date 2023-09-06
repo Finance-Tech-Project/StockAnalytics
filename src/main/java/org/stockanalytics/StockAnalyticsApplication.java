3a5986d5e3327469431af6139304974c5ec24f93
Michael Kamenetsky <mikhkamen@gmail.com>
2023-09-07 01:12:50 +0300

    by

--- src/main/java/org/stockanalytics/StockAnalyticsApplication.java
+++ src/main/java/org/stockanalytics/StockAnalyticsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class StockAnalyticsApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(StockAnalyticsApplication.class, args);
     }
