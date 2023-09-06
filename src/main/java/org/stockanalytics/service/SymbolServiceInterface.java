00bac312be6fb6bfc17c944a5b89dc07c236ee4f
Michael Kamenetsky <mikhkamen@gmail.com>
2023-09-07 00:42:27 +0300

    byby

--- src/main/java/org/stockanalytics/service/SymbolServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package org.stockanalytics.service;
-
-public interface SymbolServiceInterface {
-//
-//    int addSymbolsFromList(List<String> symbolNames);
-//
-//    List<SymbolDto> getAllSymbols();
-
-}
