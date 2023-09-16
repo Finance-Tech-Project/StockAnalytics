6eaf8b575c234605df39c743d9b386c54505c28f
Evgen Shkriba <shkribaev@gmail.com>
2023-09-16 19:00:31 +0300

    commit with postgres db

--- src/main/java/com/stockanalytics/security/model/Role.java
+++
@@ -1,5 +0,0 @@
-//package com.stockanalytics.security.model;
-//
-//public enum Role {
-//	USER, MODERATOR, ADMINISTRATOR
-//}
--- src/test/java/telran/fintech/FinTechBackEndUserApplicationTests.java
+++ src/test/java/telran/fintech/FinTechBackEndUserApplicationTests.java
@@ -1,13 +1,13 @@
-package telran.fintech;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FinTechBackEndUserApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package telran.fintech;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FinTechBackEndUserApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
