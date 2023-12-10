fd82521eaa5015643330a4c35470a58da08dc37c
Evgen Shkriba <shkribaev@gmail.com>
2023-12-10 21:32:30 +0200

    Cors security

--- src/main/java/com/stockanalytics/accounting/controller/UserAccountController.java
+++ src/main/java/com/stockanalytics/accounting/controller/UserAccountController.java
@@ -8,16 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import lombok.RequiredArgsConstructor;
 import com.stockanalytics.accounting.dto.RolesDto;
 import com.stockanalytics.accounting.dto.UserDto;
@@ -39,15 +30,17 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 public class UserAccountController {
 
 private  final UserAccountService userAccountService;
-
+@CrossOrigin
 	@PostMapping("/register")
 	public UserDto register(@RequestBody UserRegisterDto userRegisterDto) {
 		return userAccountService.register(userRegisterDto);
 	}
+	@CrossOrigin
 	@PostMapping("/login")
 	public UserDto login(Principal principal) {
 		return userAccountService.getUser(principal.getName());
 	}
+	@CrossOrigin
 	@PostMapping("/logout")
 	public ResponseEntity<String> logout(HttpServletRequest request, HttpServletResponse response) {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
--- src/main/java/com/stockanalytics/accounting/security/AuthorizationConfiguration.java
+++ src/main/java/com/stockanalytics/accounting/security/AuthorizationConfiguration.java
@@ -1,27 +1,52 @@
 package com.stockanalytics.accounting.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 
-public class AuthorizationConfiguration {
+public class AuthorizationConfiguration extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private CorsConfigurationSource corsConfigurationSource;
 
 	@Bean
-	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.httpBasic(withDefaults());
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-         http.authorizeRequests(authorize -> authorize
-						 .mvcMatchers("/**")
-						 .permitAll()
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Замените на свой фронтенд URL
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return source;
+	}
+
+
+	@Bean
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.cors().configurationSource(corsConfigurationSource) // Интеграция CORS с Spring Security
+				.and()
+				.httpBasic(withDefaults())
+				.csrf().disable()
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				.authorizeRequests(authorize -> authorize
+								.mvcMatchers("/**").permitAll()
 
 //
 //        		.mvcMatchers("/start/remove","/start/add","/service/addsymbols","/account/register","/start/symbols", "/statistics?ticker={tickerName}", "/searchSymbol/?search={searchedTickerForLetters}", "/quote/history?dateFrom={dateFrom}&dateTo={dateTo}&ticker={tickerName}")
@@ -40,6 +65,6 @@ public class AuthorizationConfiguration {
   	    .authenticated()
        );
 
-		return http.build();
+
 	}
 }
