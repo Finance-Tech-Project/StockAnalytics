48be71c8c4f19f1b406c2ba0378c460099698a20
Vladimir Kovalenko <vova.k2009@gmail.com>
2024-02-27 09:33:55 +0000

    Replace getDividendFromTable with downloadAndProcessDividends method for parsing dividend data from CSV file
    Implement the downloadAndProcessDividends method in DividendProcessor class, enabling the download of dividend data from a specified URL and parsing it into a structured list of Dividend objects. This addition facilitates automated processing of dividend information from external sources, enhancing our data analysis capabilities.

--- src/main/java/com/stockanalytics/util/HTMLParser.java
+++ src/main/java/com/stockanalytics/util/HTMLParser.java
@@ -9,7 +9,11 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -76,63 +80,75 @@ public class HTMLParser {
     }
 
     public List<Dividend> getDividend(Symbol symbol) {
-        String url1 = "https://finance.yahoo.com/quote/".concat(symbol.getName().concat("/history?period1=978307200&period2=1356912000&interval=3mo&filter=history&frequency=1d&includeAdjustedClose=true"));
-        String url2 = "https://finance.yahoo.com/quote/".concat(symbol.getName().concat("/history?period1=1356998400&period2=1735603200&interval=3mo&filter=history&frequency=1d&includeAdjustedClose=true"));
         List<Dividend> dividends = new ArrayList<>();
-        getDividendFromTable(url1, dividends, symbol);
-        getDividendFromTable(url2, dividends, symbol);
+        String url = "https://query1.finance.yahoo.com/v7/finance/download/".concat(symbol.getName().concat("?period1=0&period2=3476250418&interval=1d&events=div&includeAdjustedClose=true"));
+        dividends = downloadAndProcessDividends(url, symbol);
 
         if (dividends.isEmpty()) {
-            symbol.setHasDividends(1);
+            symbol.setHasDividends(0);
         }
         if (!dividends.isEmpty()) {
-            symbol.setHasDividends(2);
+            symbol.setHasDividends(1);
         }
 
         return dividends.stream().sorted(Comparator.comparing(Dividend::getDate))
                 .collect(Collectors.toList());
     }
 
-    private void getDividendFromTable(String url, List<Dividend> dividends, Symbol symbol) {
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd, yyyy", Locale.ENGLISH);
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(url).get();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Element table = null;
-        if (doc != null) {
-            table = doc.select("table").first();
-        }
-        Elements rows = null;
-        if (table != null) {
-            rows = table.select("tr");
-        }
 
-        if (rows != null) {
-            for (int i = 1; i < rows.size() - 1; i++) {
-                Element row = rows.get(i);
-                if (row.text().contains("Dividend")) {
-                    Elements cols = row.select("td");
-                    Element dateElement = cols.select("td:nth-of-type(1) span").first();
-                    LocalDate date = null;
-                    if (dateElement != null) {
-                        date = LocalDate.parse(dateElement.text(), formatter);
-                    }
-                    Element dividendElement = row.select("td:nth-of-type(2) strong").first();
-                    Double dividendRate = null;
-                    if (dividendElement != null) {
-                        dividendRate = Double.parseDouble(dividendElement.text());
-                    }
+    /**
+     * Downloads and processes dividend data from a specified URL, parsing it into a list of {@link Dividend} objects.
+     * <p>
+     * This method connects to the given URL, expected to return a CSV file containing dividend information, with each row
+     * representing a dividend payment. The CSV format is assumed to have at least two columns: the payment date and
+     * the dividend amount. The method parses each row of the CSV into a {@link Dividend} object, which is then added
+     * to a list of dividends.
+     * <p>
+     * Note: This method requires a stable internet connection and the ability to connect to the external URL. It also assumes
+     * that the URL points directly to a downloadable CSV file containing the relevant dividend data.
+     *
+     * @param urlString The URL from which to download the dividend data, formatted as a string.
+     * @return A List of {@link Dividend} objects, each representing a dividend payment as parsed from the downloaded CSV.
+     * @throws IOException If an input or output exception occurred during network connection or file processing.
+     */
+    public static List<Dividend> downloadAndProcessDividends(String urlString, Symbol symbol) {
+        List<Dividend> dividends = new ArrayList<>();
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            // Set User-Agent to mimic a web browser request
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
+            connection.setRequestProperty("Accept", "text/csv");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                // Assuming the first line is the header and skipping it
+                in.readLine();
+
+                while ((inputLine = in.readLine()) != null) {
+                    String[] data = inputLine.split(",");
+                    LocalDate date = LocalDate.parse(data[0]);
+                    Double amount = Double.valueOf(data[1]);
 
                     Dividend dividend = new Dividend();
-                    dividend.setDate(date);
-                    dividend.setDividendRate(dividendRate);
                     dividend.setSymbol(symbol);
+                    dividend.setDate(date);
+                    dividend.setDividendRate(amount);
                     dividends.add(dividend);
                 }
+                in.close();
+            } else {
+                System.out.println("Failed to download dividends data: HTTP error code " + responseCode);
             }
+        } catch (Exception e) {
+            System.err.println("An error occurred while downloading or processing dividends data:");
+            e.printStackTrace();
         }
+
+        return dividends;
     }
 }
